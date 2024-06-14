cca42623b748e97d7cf8c128821d8e6b6a4cfa87
Anders Jensen <apjensen@ncsu.edu>
2024-06-13 20:27:40 -0400

    update main

--- Main.py
+++ Main.py
@@ -3,7 +3,6 @@
 # Main.py
 ########################
 
-
 import pandas as pd
 import yfinance as yf
 import matplotlib.pyplot as plt
@@ -62,21 +61,65 @@ if __name__ == "__main__":
 
     SPX = Ticker('^GSPC')
     SPX_prices = SPX.get_prices('1990-01-01', '2024-05-01')
-    SPX_returns = SPX.get_returns(three_month_length, '1990-01-01', '2024-05-01')
+    SPX_returns = SPX.get_returns(month_length, '1990-01-01', '2024-05-01')
+
     SPX_VIX = Ticker.add_vix(SPX_returns)
 
     SPX_return_values = [return_tuple[1] for return_tuple in SPX_returns]
     VIX_levels = [return_tuple[2] for return_tuple in SPX_VIX]
 
-    # Create dot plot
-    plt.figure(figsize=(10, 6))
-    plt.plot(SPX_return_values, VIX_levels, 'o', color='blue')
-    plt.xlabel('SPX Returns (3 months) %')
-    plt.ylabel('VIX Prices')
-    plt.title('SPX Returns vs. VIX Prices')
-    plt.grid(True)
-    plt.show()
+    bins = int(len(SPX_return_values)**0.33333)
 
+    VIX_max = max(VIX_levels)
+    VIX_min = min(VIX_levels)
+    VIX_range = VIX_max - VIX_min
+    VIX_step = VIX_range / bins
+    VIX_intervals = []
+    for i in range(bins + 1):
+        VIX_intervals.append([VIX_min + VIX_step * i, VIX_min + VIX_step * (i + 1)])
+
+    SPX_interval_values = []
+    for v in VIX_intervals:
+        interval_values = []
+        for i in range(len(SPX_return_values)):
+            if v[0] <= VIX_levels[i] < v[1]:
+                interval_values.append(SPX_return_values[i])
+        SPX_interval_values.append(interval_values)
+
+    print("--" * 30)
+    print(bins)
+    while True:
+        index = int(input("Enter range index: "))
+        if 0 <= index < len(VIX_intervals):
+            print(SPX_interval_values[index])
+            interval_bins = int(len(SPX_interval_values[index])**0.5)
+            plt.figure(figsize=(10, 6))
+            plt.hist(SPX_interval_values[index], bins=interval_bins, color='blue', edgecolor='black')
+            plt.xlabel('SPX Returns (1 month) %')
+            plt.ylabel('Frequency')
+            plt.title(f'SPX Returns - VIX Interval: {VIX_intervals[index]}')
+            plt.grid(True)
+            plt.show()
+        else:
+            break
+
+    # Histogram
+    # plt.figure(figsize=(10, 6))
+    # plt.hist(SPX_return_values, bins=bins, color='blue', edgecolor='black')
+    # plt.xlabel('SPX Returns (1 month) %')
+    # plt.ylabel('Frequency')
+    # plt.title('SPX Returns')
+    # plt.grid(True)
+    # plt.show()
+
+    # Dot plot
+    # plt.figure(figsize=(10, 6))
+    # plt.plot(SPX_return_values, VIX_levels, 'o', color='blue')
+    # plt.xlabel('SPX Returns (1 month) %')
+    # plt.ylabel('VIX Level')
+    # plt.title('SPX Returns vs. VIX Level')
+    # plt.grid(True)
+    # plt.show()
 
 
 
